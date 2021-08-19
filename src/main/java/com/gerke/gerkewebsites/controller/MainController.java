4008f25fdb95e42697ebca9fbebbb9fffb414a00
Piotr Gerke <piotrg2003@gmail.com>
2021-08-19 10:53:36 +0200

    added initial request

--- src/main/java/com/gerke/gerkewebsites/controller/MainController.java
+++ src/main/java/com/gerke/gerkewebsites/controller/MainController.java
@@ -10,12 +10,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @Autowired
     EmailService emailService;
+
+    @RequestMapping("/")
+    public String home() {
+        return "index.html";
+    }
 
     @GetMapping("/")
     public String homePage(Model model) {
