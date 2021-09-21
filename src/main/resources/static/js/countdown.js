e3cbe9fee3a9f40be8b90e8c91bbfc2afb6d456f
Piotr Gerke <piotrg2003@gmail.com>
2021-09-21 07:12:52 +0200

    countdown update

--- src/main/resources/static/js/countdown.js
+++ src/main/resources/static/js/countdown.js
@@ -1,4 +1,4 @@
-const destination = new Date("sep 17, 2021 22:00:00").getTime();
+const destination = new Date("sep 30, 2021 22:00:00").getTime();
 setInterval(function(){
 let now = new Date().getTime();
 
